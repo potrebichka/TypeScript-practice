a5b5c2263b85480122ded98d00b08ca2a699566c
Nina Potrebich <potrebich@gmail.com>
2020-04-15 11:33:02 -0700

    Init project

---
+++ app.ts
@@ -0,0 +1,28 @@
+function combine (
+    input1: number | string, 
+    input2: number | string, 
+    resultConversion: 'as-number' | 'as-text'
+) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 +  +input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
+        
+    return result;
+}
+
+const combineAges = combine(30,26, 'as-number');
+console.log(combineAges);
+
+const combineStringAges =combine('30', '26', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Alex', 'Anna', 'as-text');
+console.log(combineNames);
