1e1636c0f8944e8a7065e1e23b0836f7279f3a13
Nina Potrebich <potrebich@gmail.com>
2020-04-23 21:49:48 -0700

    Unknown type

--- app.ts
+++ app.ts
@@ -1,15 +1,8 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max',
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-
-function printResult(num: number): void {
-    console.log('Result ' + num);
-}
-
-printResult(add(5,12));
-
-let combineValues: (a: number, b:number) => number;
- 
-combineValues = add;
-
-console.log(combineValues(8,8));
